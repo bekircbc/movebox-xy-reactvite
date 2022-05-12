7bb4d62a456f95e0bd61c852599a3f09d2497e89
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-12 13:42:52 +0200

    moving box finished. up, down, right, left moving..

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,50 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
-  const [xPosition, setXPosition] = useState(250);
-  const [yPosition, setYPosition] = useState(250);
+  const [xPosition, setXPosition] = useState(275);
+  const [yPosition, setYPosition] = useState(275);
 
-  useEffect(() => {}, [xPosition, yPosition]);
   return (
     <div className="App">
       <h2>Move an element left, right, center when button is pressed</h2>
       <div className="controls">
         <div className="row">
-          <button className="up" onClick={() => setYPosition(yPosition - 2)}>
+          <button className="up" onClick={() => setYPosition(yPosition - 5)}>
             Up
           </button>
         </div>
         <div className="row">
-          <button className="left" onClick={() => setXPosition(xPosition - 2)}>
+          <button className="left" onClick={() => setXPosition(xPosition + 5)}>
             Left
           </button>
           <button
             className="center"
             onClick={() => {
-              setXPosition(250);
-              setYPosition(250);
+              setXPosition(275);
+              setYPosition(275);
             }}
           >
             Center
           </button>
-          <button className="right" onClick={() => setXPosition(xPosition + 2)}>
+          <button className="right" onClick={() => setXPosition(xPosition - 5)}>
             Right
           </button>
         </div>
         <div className="row">
-          <button className="down" onClick={() => setYPosition(xPosition + 2)}>
+          <button className="down" onClick={() => setYPosition(yPosition + 5)}>
             Down
           </button>
         </div>
-
-        <div className="gameArea">
-          <div
-            className="box"
-            style={({ top: `${xPosition}px)` }, { right: `${xPosition}px)` })}
-          ></div>
-        </div>
+      </div>
+      <div className="gameArea">
+        <div
+          className="box"
+          style={{ top: `${yPosition}px`, right: `${xPosition}px` }}
+        ></div>
       </div>
     </div>
   );
 }
 
 export default App;
-
-// function App() {
-//   const [pos, setPos] = useState(0);
-
-//   return (
-//     <div className="spa006">
-//       <h2>Spa006 - Move element up on button click</h2>
-//       <button onClick={() => setPos(pos - 10)}>Move up</button>
-//       <div
-//         className="theElement"
-//
-//       ></div>
-//     </div>
-//   );
-// }
-
-// export default App;
