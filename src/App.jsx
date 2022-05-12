40cbf3bde389183778e0b73823029d4910110a83
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-12 10:45:51 +0200

    styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="App">Hallo!</div>;
+  const [pos, setPos] = useState(0);
+
+  return (
+    <div className="spa006">
+      <h2>Spa006 - Move element up on button click</h2>
+      <button onClick={() => setPos(pos - 10)}>Move up</button>
+      <div
+        className="theElement"
+        style={{ transform: `translateY(${pos}px)` }}
+      ></div>
+    </div>
+  );
 }
 
 export default App;
