c63abf02fe75ec428c6363ff8b928b2ab76b5278
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-12 10:48:22 +0200

    left, center and right movement with flex-start, center, flex-end succeeded.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,62 @@ import { useState } from "react";
 import "./App.scss";
 
 function App() {
-  const [pos, setPos] = useState(0);
-
+  const [xPosition, setXPosition] = useState("Center");
+  const [yPosition, setYPosition] = useState("");
   return (
-    <div className="spa006">
-      <h2>Spa006 - Move element up on button click</h2>
-      <button onClick={() => setPos(pos - 10)}>Move up</button>
-      <div
-        className="theElement"
-        style={{ transform: `translateY(${pos}px)` }}
-      ></div>
+    <div className="App">
+      <h2>Move an element left, right, center when button is pressed</h2>
+      <div className="controls">
+        <div className="row">
+          <button className="up" onClick={() => setYPosition("Up")}>
+            Up
+          </button>
+        </div>
+        <div className="row">
+          <button className="left" onClick={() => setXPosition("Left")}>
+            Left
+          </button>
+          <button
+            className="center"
+            onClick={() => {
+              setXPosition("Center");
+            }}
+          >
+            Center
+          </button>
+          <button className="right" onClick={() => setXPosition("Right")}>
+            Right
+          </button>
+        </div>
+        <div className="row">
+          <button className="down" onClick={() => setYPosition("Down")}>
+            Down
+          </button>
+        </div>
+
+        <div className={`gameArea gameArea${xPosition} gameArea${yPosition}`}>
+          <div className="box"></div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// function App() {
+//   const [pos, setPos] = useState(0);
+
+//   return (
+//     <div className="spa006">
+//       <h2>Spa006 - Move element up on button click</h2>
+//       <button onClick={() => setPos(pos - 10)}>Move up</button>
+//       <div
+//         className="theElement"
+//         style={{ transform: `translateY(${pos}px)` }}
+//       ></div>
+//     </div>
+//   );
+// }
+
+// export default App;
